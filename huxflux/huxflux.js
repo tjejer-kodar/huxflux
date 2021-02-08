6dc32560d6cc6ae23ddfb34821a2c64dbdb69690
Evelinn <evelinn.idenfors@gmail.com>
2021-02-08 13:27:42 +0100

    solution upload

--- huxflux/huxflux.js
+++ huxflux/huxflux.js
@@ -1,8 +1,22 @@
 /**
- * @returns {Number[]} Lista med sju nummer
+ * @returns {Number[]}
  */
 function huxflux() {
-    return; // Returnera din lista med nummer här
+    let Number = [];
+
+    let recursion = () => {
+        let newNum = Math.floor(Math.random() * 36);
+        if (Number.indexOf(newNum) == -1 && newNum !== 0) {
+            return newNum;
+        } else {
+            return recursion();
+        }
+    }
+
+    for (let i = 0; i < 7; i++) {
+            Number.push(recursion());
+    }
+    return Number;
 }
 
 export {
