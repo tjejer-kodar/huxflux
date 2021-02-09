b980c836d58d7a5c4096274e9c6eea019dd2203e
Amanda Araskog <73289643+araskog@users.noreply.github.com>
2021-02-09 10:34:18 +0100

    Add files via upload

--- huxflux/huxflux.js
+++ huxflux/huxflux.js
@@ -1,10 +1,22 @@
-/**
- * @returns {Number[]} Lista med sju nummer
- */
-function huxflux() {
-    return; // Returnera din lista med nummer här
-}
+// Huxflux returns an array of 7 unique integers
+const huxflux = () => {
+  // Generate a random integer
+  const randomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 
-export {
-    huxflux
+  // Set to contain unique integers
+  let list = new Set();
+
+  // Fill the set with 7 unique integers between 1 and 35
+  do {
+    let heltal = randomInt(1, 35);
+    list.add(heltal);
+  } while (list.size < 7);
+
+  return [...list];
 };
+
+export { huxflux };
